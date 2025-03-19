c5ababfd61cdfb3cb9e270d6bfad7433c3e18641
vainga <64656614+vainga@users.noreply.github.com>
2025-03-19 20:09:16 +0700

    Work master

--- TFLaComp-1/ParserForm.cs
+++ TFLaComp-1/ParserForm.cs
@@ -1,46 +1,103 @@
-using System.DirectoryServices;
 using System.Windows.Forms;
 using TFLaComp_1.Functional;
 using TFLaComp_1.ParserHelp;
-using TFLaComp_1.RegExParser;
 
 namespace TFLaComp_1
 {
     public partial class ParserForm : System.Windows.Forms.Form
     {
         private IEdit _edit;
+
         private IFileLogic _logic;
+
         private IParserHelpProvider _helpProvider;
+
         private bool isTextChanged = false;
 
         public ParserForm()
         {
             InitializeComponent();
+
+
             this.HelpButton = true;
 
             _edit = new Edit(richTextBoxInput);
             _logic = new FileLogic();
-            _helpProvider = new ParserHelpProvider();
+            //_helpProvider = new ParserHelpProvider($"\\ParserHelp\\res\\parserHelpProvider.chm");
+            //helpProvider1 = _helpProvider.HelpProvider;
+            //helpProvider1.HelpNamespace = $"ParserHelp\\res\\parserHelpProvider.chm";
 
+
+            _helpProvider = new ParserHelpProvider();
             _helpProvider.SetHelp(richTextBoxInput, HelpHtmDict.TopicDict["Правка"], HelpNavigator.Topic);
             _helpProvider.SetHelp(file, "Создать", HelpNavigator.KeywordIndex);
             _helpProvider.SetHelp(open, "Открыть", HelpNavigator.KeywordIndex);
             _helpProvider.SetHelp(save, "Сохранить", HelpNavigator.KeywordIndex);
             _helpProvider.SetHelp(start, "Пуск", HelpNavigator.Topic);
+
             _helpProvider.SetHelp(undo, "Отменить", HelpNavigator.KeywordIndex);
             _helpProvider.SetHelp(redo, "Повторить", HelpNavigator.KeywordIndex);
             _helpProvider.SetHelp(copy, "Копировать", HelpNavigator.KeywordIndex);
             _helpProvider.SetHelp(cut, "Вырезать", HelpNavigator.KeywordIndex);
             _helpProvider.SetHelp(paste, "Вставить", HelpNavigator.KeywordIndex);
+            //_helpProvider.SetHelp(, "Удалить", HelpNavigator.KeywordIndex);
+            //_helpProvider.SetHelp(, "Выделить все", HelpNavigator.KeywordIndex);
+
             _helpProvider.SetHelp(this, "О программе", HelpNavigator.KeywordIndex);
 
+            // добавить какой-то Binding?
             helpProvider1 = _helpProvider.HelpProvider;
+
             richTextBoxInput.TextChanged += richTextBoxInput_TextChanged;
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void makeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // добавить Binding
+            string text = richTextBoxInput.Text;
+            _logic.Create(ref text);
+            //richTextBoxInput.Text = "";
+            richTextBoxInput.Text = text;
+            richTextBoxOutput.Text = "";
+        }
+
+        private void file_Click(object sender, EventArgs e)
+        {
+            if (ConfirmSaveChanges())
+            {
+                // добавить Binding
+                string text = richTextBoxInput.Text;
+                _logic.Create(ref text);
+                //richTextBoxInput.Text = "";
+                richTextBoxInput.Text = text;
+                richTextBoxOutput.Text = "";
+            }
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            if (ConfirmSaveChanges())
+            {
+                LoadFile();
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ConfirmSaveChanges())
+            {
+                LoadFile();
+            }
         }
 
         private bool ConfirmSaveChanges()
         {
-            if (!isTextChanged) return true;
+            if (!isTextChanged) return true; // Если изменений нет, просто продолжаем.
 
             DialogResult result = MessageBox.Show(
                 "Сохранить изменения перед открытием нового файла?",
@@ -54,7 +111,12 @@ namespace TFLaComp_1
                 isTextChanged = false;
                 return true;
             }
-            return result == DialogResult.No;
+            else if (result == DialogResult.No)
+            {
+                return true;
+            }
+
+            return false; // Отмена операции
         }
 
         private void LoadFile()
@@ -66,7 +128,7 @@ namespace TFLaComp_1
                     throw new FileLoadException("Ошибка открытия файла!");
 
                 richTextBoxInput.Text = text;
-                isTextChanged = false;
+                isTextChanged = false; // Сбрасываем флаг изменений
             }
             catch (Exception ex)
             {
@@ -74,42 +136,173 @@ namespace TFLaComp_1
             }
         }
 
-        private void HighlightResults(List<CardDTO> cards)
+        private void save_Click(object sender, EventArgs e)
         {
-            richTextBoxInput.SelectAll();
-            richTextBoxInput.SelectionColor = richTextBoxInput.ForeColor;
-            richTextBoxInput.SelectionFont = richTextBoxInput.Font;
-
-            foreach (var card in cards)
-            {
-                richTextBoxInput.Select(card.IndexStart, card.IndexEnd - card.IndexStart + 1);
-                richTextBoxInput.SelectionColor = card.NumberCard[0] switch
-                {
-                    '2' => Color.Green,
-                    '4' => Color.Blue,
-                    '5' => Color.OrangeRed,
-                    _ => Color.Purple
-                };
-                richTextBoxInput.SelectionFont = new Font(richTextBoxInput.Font, FontStyle.Bold);
-            }
+            _logic.Save(richTextBoxInput.Text);
         }
 
-        private void ProcessInput()
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var parser = new CardParser();
-            var results = parser.Parse(richTextBoxInput.Text);
-            HighlightResults(results);
+            _logic.Save(richTextBoxInput.Text);
         }
 
-        private void start_Click(object sender, EventArgs e) => ProcessInput();
-        private void StartToolStripMenuItem_Click(object sender, EventArgs e) => ProcessInput();
-        private void undo_Click(object sender, EventArgs e) => _edit.Undo();
-        private void redo_Click(object sender, EventArgs e) => _edit.Redo();
-        private void cut_Click(object sender, EventArgs e) => _edit.Cut();
-        private void paste_Click(object sender, EventArgs e) => _edit.Paste();
-        private void copy_Click(object sender, EventArgs e) => _edit.Copy();
+        private void saveAssToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _logic.SaveAs(richTextBoxInput.Text);
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Нажат старт");
+        }
+        private void StartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Нажат старт");
+        }
+
+        private void undo_Click(object sender, EventArgs e)
+        {
+            _edit.Undo();
+        }
+
+        private void redo_Click(object sender, EventArgs e)
+        {
+            _edit.Redo();
+        }
+
+        private void cut_Click(object sender, EventArgs e)
+        {
+            _edit.Cut();
+        }
+
+        private void paste_Click(object sender, EventArgs e)
+        {
+            _edit.Paste();
+        }
+
+        private void copy_Click(object sender, EventArgs e)
+        {
+            _edit.Copy();
+        }
+
+        private void callHelpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Help.ShowHelp(this, helpProvider1.HelpNamespace);
+        }
+
+        private void aboutCodeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Help.ShowHelp(this, helpProvider1.HelpNamespace, HelpNavigator.KeywordIndex, "О программе");
+        }
+
+        private void expToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // потом должен открываться файл курсовой
+            Help.ShowHelp(this, helpProvider1.HelpNamespace,
+                HelpNavigator.Topic, HelpHtmDict.TopicDict["Постановка задачи"]);
+        }
+
+        private void grammarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // потом должен открываться файл курсовой
+            Help.ShowHelp(this, helpProvider1.HelpNamespace,
+                HelpNavigator.Topic, HelpHtmDict.TopicDict["Грамматика"]);
+
+        }
+
+        private void classificationgrammarClassToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // потом должен открываться файл курсовой
+            Help.ShowHelp(this, helpProvider1.HelpNamespace,
+                HelpNavigator.Topic, HelpHtmDict.TopicDict["Классификация"]);
+
+        }
+
+        private void analysismethodToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // потом должен открываться файл курсовой
+            Help.ShowHelp(this, helpProvider1.HelpNamespace,
+                HelpNavigator.Topic, HelpHtmDict.TopicDict["Метод анализа"]);
+
+        }
+
+        private void diagnosticsNeutralizationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // потом должен открываться файл курсовой
+            Help.ShowHelp(this, helpProvider1.HelpNamespace,
+                HelpNavigator.Topic, HelpHtmDict.TopicDict["Диагностика"]);
+
+        }
+
+        private void explToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // потом должен открываться файл курсовой
+            Help.ShowHelp(this, helpProvider1.HelpNamespace,
+                HelpNavigator.Topic, HelpHtmDict.TopicDict["Тестовый пример"]);
+
+        }
+
+        private void bibliographyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // потом должен открываться файл курсовой
+            Help.ShowHelp(this, helpProvider1.HelpNamespace,
+                HelpNavigator.Topic, HelpHtmDict.TopicDict["Список литературы"]);
+
+        }
+
+        private void sourceCodeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // потом должен открываться файл курсовой
+            Help.ShowHelp(this, helpProvider1.HelpNamespace,
+                HelpNavigator.Topic, HelpHtmDict.TopicDict["Исходный код"]);
+
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _edit.Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _edit.Redo();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _edit.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _edit.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _edit.Paste();
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _edit.Delete();
+        }
+
+        private void pasteAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _edit.SelectAll();
+        }
+
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            ////////////////////////////////////
+            ///
+            /*            DialogResult dr = MessageBox.Show("Сохранить внесенные изменения перед выходом?", "Сохранение", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                        if (dr.HasFlag(DialogResult.Yes))
+                            _logic.SaveAs(richTextBoxInput.Text);
+                        else if (dr.HasFlag(DialogResult.No))
+                            _logic.Close();*/
+
             DialogResult result = MessageBox.Show(
                 "Вы хотите сохранить перед выходом?",
                 "Подтверждение",
@@ -122,10 +315,21 @@ namespace TFLaComp_1
             }
             else if (result == DialogResult.No)
             {
+                //_logic.Close();
                 Application.Exit();
             }
         }
 
-        private void richTextBoxInput_TextChanged(object sender, EventArgs e) => isTextChanged = true;
+        private void richTextBoxInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            _edit.SaveUndo();
+        }
+
+        private void richTextBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            isTextChanged = true;
+
+        }
+
     }
 }
