c17a2b7dec0c6e1de07f755536939c6ee3f77307
vainga <64656614+vainga@users.noreply.github.com>
2025-03-19 22:11:45 +0700

    Add AnalyzerCard and FullCardDTO

---
+++ TFLaComp-1/RegExParser/FullCardDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFLaComp_1.RegExParser
+{
+    public class FullCardDTO
+    {
+        private readonly CardDTO _card;
+
+        public string PaymentSystem {  get; private set; }
+        public string Bank { get; private set; }
+
+        public FullCardDTO(CardDTO card, string paymentSystem, string bank)
+        { 
+            _card = card; 
+            PaymentSystem = paymentSystem;
+            Bank = bank;
+        }
+
+
+
+
+    }
+}
