f5d73eaf8bb6754670e47cd64d7f862d250ef78a
Nikita Lebedko <lebedkonikita402@gmail.com>
2025-05-15 21:17:00 +0700

    LastVersion

--- TFLaComp-1/CardParser/Tokenizer.cs
+++ TFLaComp-1/CardParser/Tokenizer.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using TFLaComp_1.CardParser.Interface;
-
 namespace TFLaComp_1.CardParser
 {
     public class Tokenizer : ITokenizer
     {
         public List<string> Tokens { get; } = new();
         public List<SyntaxError> Errors { get; } = new();
-
         IEnumerable<ISyntaxError> ITokenizer.Errors => Errors;
 
         public Tokenizer(string input)
@@ -18,6 +17,7 @@ namespace TFLaComp_1.CardParser
         private void Tokenize(string input)
         {
             int i = 0;
+
             while (i < input.Length)
             {
                 char c = input[i];
@@ -28,71 +28,90 @@ namespace TFLaComp_1.CardParser
                     continue;
                 }
 
+                // <<
                 if (c == '<' && i + 1 < input.Length && input[i + 1] == '<')
                 {
                     Tokens.Add("<<");
                     i += 2;
                     continue;
                 }
-                else if (c == '>' && i + 1 < input.Length && input[i + 1] == '<')
-                {
-                    Errors.Add(new SyntaxError(i, "Недопустимая последовательность '><'", ErrorType.Lexical));
-                    i += 2;
-                    continue;
-                }
-                else if (c == '<')
+
+                // одиночный <
+                if (c == '<')
                 {
                     Errors.Add(new SyntaxError(i, "Одиночный символ '<' недопустим. Используйте '<<'", ErrorType.Lexical));
                     i++;
                     continue;
                 }
-                else if (c == ';')
+
+                // ;
+                if (c == ';')
                 {
                     Tokens.Add(";");
                     i++;
                     continue;
                 }
-                else if (char.IsLetter(c))
+
+                // потенциальный идентификатор (с одним ошибочным символом)
+                if (IsLatinLetter(c) || c == '_')
                 {
-                    string id = "";
-                    while (i < input.Length && (char.IsLetter(input[i]) || input[i] == '_' || char.IsDigit(input[i])))
+                    int start = i;
+                    StringBuilder buffer = new();
+                    bool errorLogged = false;
+
+                    while (i < input.Length)
                     {
-                        id += input[i];
-                        i++;
+                        char ch = input[i];
+
+                        if (IsLatinLetter(ch) || char.IsDigit(ch) || ch == '_')
+                        {
+                            buffer.Append(ch);
+                            i++;
+                        }
+                        else if (!errorLogged && IsInvalidInIdentifier(ch))
+                        {
+                            Errors.Add(new SyntaxError(i, $"Недопустимый символ: '{ch}'", ErrorType.Lexical));
+                            errorLogged = true;
+                            i++; // Пропускаем один недопустимый символ
+                        }
+                        else
+                        {
+                            break;
+                        }
                     }
-                    Tokens.Add(id);
+
+                    if (buffer.Length > 0)
+                        Tokens.Add(buffer.ToString());
+
                     continue;
                 }
-                else if (char.IsDigit(c))
+
+                // числа — ошибка
+                if (char.IsDigit(c))
                 {
                     int start = i;
                     string number = "";
+
                     while (i < input.Length && char.IsDigit(input[i]))
                     {
                         number += input[i];
                         i++;
                     }
+
                     Errors.Add(new SyntaxError(start, $"Недопустимая лексема: {number}", ErrorType.Lexical));
                     continue;
                 }
-                else
-                {
-                    int start = i;
-                    string invalid = "";
-                    while (i < input.Length &&
-                           !char.IsWhiteSpace(input[i]) &&
-                           !char.IsLetter(input[i]) &&
-                           !char.IsDigit(input[i]) &&
-                           input[i] != '<' &&
-                           input[i] != ';')
-                    {
-                        invalid += input[i];
-                        i++;
-                    }
-                    Errors.Add(new SyntaxError(start, $"Недопустимая лексема: {invalid}", ErrorType.Lexical));
-                    continue;
-                }
+
+                // всё остальное — просто ошибка
+                Errors.Add(new SyntaxError(i, $"Недопустимый символ: '{c}'", ErrorType.Lexical));
+                i++;
             }
         }
+
+        private bool IsLatinLetter(char c) =>
+            (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+
+        private bool IsInvalidInIdentifier(char c) =>
+            "!@#$%^&*()+=[]{}\\|:,.?/`~\"'".Contains(c);
     }
 }
